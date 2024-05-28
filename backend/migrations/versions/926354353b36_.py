8b1bff27fedc943c55f1fdbdfc5765a6b26e4bb1
masayukikudo <masayuki.kudo0307@gmail.com>
2024-05-28 21:14:39 +0900

    Update files to match local changes

---
+++ backend/migrations/versions/926354353b36_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 926354353b36
+Revises: 
+Create Date: 2024-04-07 14:04:44.379228
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '926354353b36'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
