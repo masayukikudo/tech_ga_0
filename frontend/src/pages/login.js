b09a329f829dc1e209db3f0495127706477948de
masayukikudo <masayuki.kudo0307@gmail.com>
2024-07-27 18:38:35 +0900

    ルーティング修正

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -8,39 +8,20 @@ function Login() {
     const [rememberMe, setRememberMe] = useState(false);
     const [managerRedirect, setManagerRedirect] = useState(false);
 
-    const handleLogin = async () => {
+    const handleLogin = () => {
         console.log('Login button clicked');
-        try {
-            const response = await fetch('http://127.0.0.1:5000/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username, password, manager_redirect: managerRedirect }),
-            });
-
-            if (response.ok) {
-                const data = await response.json();
-                router.push(data.redirectURL);
-            } else {
-                const errorData = await response.json();
-                throw new Error(errorData.error);
-            }
-        } catch (error) {
-            console.error('There has been a problem with your fetch operation:', error);
-        }
+        router.push('/');  // ログインボタンをクリックすると、/index.js ページにルーティング
     };
 
     const isFormValid = username.length > 0 && password.length > 0;
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center h-screen p-4">
             <div className="text-center mb-10">
-                <h1 className="text-9xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Voices</h1>
-            　　 <p className="text-3xl mt-5">エンゲージメントサーベイへようこ
-            </p>
+                <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Voices</h1>
+                <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl mt-5">エンゲージメントサーベイへようこそ</p>
             </div>
-            <div className="mb-5 w-1/2 flex justify-center items-center">
+            <div className="mb-5 w-full sm:w-3/4 md:w-2/3 lg:w-1/2 flex justify-center items-center">
                 <div className="w-full">
                     <div className="mb-2">個人ID</div>
                     <input
@@ -48,11 +29,11 @@ function Login() {
                         id="email"
                         value={username}
                         onChange={(e) => setId(e.target.value)}
-                        className="bg-gray-200 h-12 rounded-lg p-2.5 box-border w-full text-2xl"
+                        className="bg-gray-200 h-12 rounded-lg p-2.5 box-border w-full text-lg sm:text-xl md:text-2xl"
                     />
                 </div>
             </div>
-            <div className="mb-5 w-1/2 flex justify-center items-center">
+            <div className="mb-5 w-full sm:w-3/4 md:w-2/3 lg:w-1/2 flex justify-center items-center">
                 <div className="w-full">
                     <div className="mb-2">パスワード</div>
                     <input
@@ -60,11 +41,11 @@ function Login() {
                         id="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
-                        className="bg-gray-200 h-12 rounded-lg p-2.5 box-border w-full text-2xl"
+                        className="bg-gray-200 h-12 rounded-lg p-2.5 box-border w-full text-lg sm:text-xl md:text-2xl"
                     />
                 </div>
             </div>
-            <div className="mb-5 w-4/5 flex justify-center">
+            <div className="mb-5 w-full sm:w-3/4 md:w-2/3 lg:w-1/2 flex justify-center">
                 <label className="flex items-center mr-5">
                     <input
                         type="checkbox"
